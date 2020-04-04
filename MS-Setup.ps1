4a63235dc958eb73800b952034d5efee9c3b9663
Javier Saldaña <cjaviersaldana@outlook.com>
2020-04-04 14:17:05 -0500

    Update MS-Setup.ps1

--- MS-Setup.ps1
+++ MS-Setup.ps1
@@ -3,12 +3,14 @@ choco install 7zip.install -y
 choco install sharex -y 
 choco install fiddler -y
 choco install postman -y
+choco install microsoft-windows-terminal -y
+choco install fiddler -y
 
 #code editor
 choco install vscode -y 
 choco install sql-server-management-studio -y 
 choco install azure-data-studio -y
-choco install visualstudio2019professional -y
+choco install visualstudio2015enterprise -y
 
 #environment
 choco install gittfs -y
